f31d2cb8ba1af5c5d4bc1bb89cd03cf2e6aa8a94
onurtaskirancom <taskiranonur9@gmail.com>
2022-12-06 17:23:13 +0300

    Project initialization

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const userRouter = require('./routes/user')
+
+const app = express();
+app.use(userRouter);
+
+
+
+
+app.get('/about', (req, res) => {
+    res.send('<h1>Hello World! I am from you backend about</h1>');
+});
+
+app.listen(8000, () => {
+    console.log("the port is listening on port 8000");
+})
---
+++ backend/controllers/user.js
@@ -0,0 +1,5 @@
+exports.createUser = (req, res) => {
+  res.send("<h1>I will create later.</h1>");
+};
+
+module.exports = {createUser};
