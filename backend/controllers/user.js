79b1de9eb6f23966d1006b09f8857c9c77e7f280
onurtaskirancom <taskiranonur9@gmail.com>
2022-12-15 18:29:43 +0300

    Sending Verification Email

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,23 +1,51 @@
-const User = require('../models/user')
 
-exports.create = async (req, res) => {
-  const { name, email, password } = req.body
-
-  const oldUser = await User.findOne({ email });
-
-  if (oldUser) return res.status(401).json({ error: "This email is already in use!" });
-
-  const newUser = new User({ name, email, password })
-  await newUser.save();
-
-  // var transport = nodemailer.createTransport({
-  //   host: "smtp.mailtrap.io",
-  //   port: 2525,
-  //   auth: {
-  //     user: "bf207d4e61cd81",
-  //     pass: "d8c74c2133f6b0"
-  //   }
-  // });
-
-  res.status(201).json({ user: newUser })
-};
+  const nodemailer = require('nodemailer')
+  const User = require('../models/user')
+  const EmailVerificationToken = require('../models/emailVerificationToken')
+  
+  exports.create = async (req, res) => {
+    const { name, email, password } = req.body
+  
+    const oldUser = await User.findOne({ email });
+  
+    if (oldUser) return res.status(401).json({ error: "This email is already in use!" });
+  
+    const newUser = new User({ name, email, password })
+    await newUser.save()
+  
+    // generate 6 digit otp
+    let OTP = "";
+    for (let i = 1; i <= 5; i++) {
+      const randomVal = Math.round(Math.random() * 9);
+      OTP += randomVal;
+    }
+  
+    // store otp inside our db
+    const newEmailVerificationToken = new EmailVerificationToken({ owner: newUser._id, token: OTP })
+  
+    await newEmailVerificationToken.save()
+  
+    // send that otp to our user
+  
+   var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "bf207d4e61cd81",
+      pass: "d8c74c2133f6b0"
+    }
+  });
+  
+    transport.sendMail({
+      from: 'onurtaskirannet@gmail.com',
+      to: newUser.email,
+      subject: 'Email Verification',
+      html: `
+        <p>You verification OTP</p>
+        <h1>${OTP}</h1>
+      `
+    })
+  
+    res.status(201).json({ message: 'Please verify you email. OTP has been sent to your email account!' })
+  };
+  
