f4c6ee9f8145517384f0023212b6591fe13b7fdb
onurtaskirancom <taskiranonur9@gmail.com>
2022-12-23 13:58:51 +0300

    Middleware to Check Reset Pass Token

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,6 @@
 const express = require("express");
-const {
-  create,
-  verifyEmail,
-  resendEmailVerificationToken,
-  forgetPassword,
-} = require("../controllers/user");
+const { create, verifyEmail, resendEmailVerificationToken, forgetPassword } = require("../controllers/user");
+const { isValidPassResetToken } = require("../middlewares/user");
 const { userValidtor, validate } = require("../middlewares/validator");
 
 const router = express.Router();
@@ -12,6 +8,9 @@ const router = express.Router();
 router.post("/create", userValidtor, validate, create);
 router.post("/verify-email", verifyEmail);
 router.post("/resend-email-verification-token", resendEmailVerificationToken);
-router.post("/forget-password", forgetPassword);
+router.post('/forget-password', forgetPassword)
+router.post('/verify-pass-reset-token', isValidPassResetToken, (req, res) => {
+    res.json({ valid: true })
+})
 
 module.exports = router;
