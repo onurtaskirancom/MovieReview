a23f5127cfbb4e85432a59ff6b55ba415fdcb8c7
onurtaskirancom <taskiranonur9@gmail.com>
2022-12-24 18:19:33 +0300

    Changing The User Password

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { create, verifyEmail, resendEmailVerificationToken, forgetPassword } = require("../controllers/user");
+const { create, verifyEmail, resendEmailVerificationToken, forgetPassword, sendResetPasswordTokenStatus, resetPassword } = require("../controllers/user");
 const { isValidPassResetToken } = require("../middlewares/user");
-const { userValidtor, validate } = require("../middlewares/validator");
+const { userValidtor, validate, validatePassword } = require("../middlewares/validator");
 
 const router = express.Router();
 
@@ -9,8 +9,7 @@ router.post("/create", userValidtor, validate, create);
 router.post("/verify-email", verifyEmail);
 router.post("/resend-email-verification-token", resendEmailVerificationToken);
 router.post('/forget-password', forgetPassword)
-router.post('/verify-pass-reset-token', isValidPassResetToken, (req, res) => {
-    res.json({ valid: true })
-})
+router.post('/verify-pass-reset-token', isValidPassResetToken, sendResetPasswordTokenStatus)
+router.post('/reset-password', validatePassword, validate, isValidPassResetToken, resetPassword)
 
 module.exports = router;
