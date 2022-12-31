841a33151cdbcb78d6f46ce714c89cc854a9a580
onurtaskirancom <taskiranonur9@gmail.com>
2022-12-31 17:08:15 +0300

    Sign in Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import Signin from "./components/auth/Signin";
+import Navbar from "./components/user/Navbar";
 
 export default function App() {
-    return (
-        <div>App</div>
-    )
+  return (
+    <>
+      <Navbar />
+      <Signin />
+    </>
+  );
 }
