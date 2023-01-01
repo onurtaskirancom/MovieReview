95156bdca0b4096d96be8df64e6a462742c5bb43
onurtaskirancom <taskiranonur9@gmail.com>
2023-01-02 01:27:59 +0300

    Finalizing Signin Form

--- frontend/src/components/auth/Signin.jsx
+++ frontend/src/components/auth/Signin.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "../Container";
 import FormInput from "../form/FormInput";
+import Submit from "../form/Submit";
 import Title from "../form/Title";
 
 export default function Signin() {
@@ -11,6 +12,22 @@ export default function Signin() {
           <Title>Sign in</Title>
           <FormInput label="Email" placeholder="john@email.com" name="email" />
           <FormInput label="Password" placeholder="********" name="password" />
+          <Submit value="Sign in" />
+
+          <div className="flex justify-between">
+            <a
+              className="text-dark-subtle hover:text-white transition"
+              href="#"
+            >
+              Forget password
+            </a>
+            <a
+              className="text-dark-subtle hover:text-white transition"
+              href="#"
+            >
+              Sign up
+            </a>
+          </div>
         </form>
       </Container>
     </div>
