9b305116cc3094bb5305952122f348fbe01d3511
onurtaskirancom <taskiranonur9@gmail.com>
2023-01-10 08:46:55 +0300

    Moving to Previous From Empty OTP Field

--- frontend/src/components/auth/EmailVerification.jsx
+++ frontend/src/components/auth/EmailVerification.jsx
@@ -4,6 +4,7 @@ import Submit from "../form/Submit";
 import Title from "../form/Title";
 
 const OTP_LENGTH = 6;
+let currentOTPIndex;
 
 export default function EmailVerification() {
   const [otp, setOtp] = useState(new Array(OTP_LENGTH).fill(""));
@@ -22,14 +23,21 @@ export default function EmailVerification() {
     setActiveOtpIndex(nextIndex);
   };
 
-  const handleOtpChange = ({ target }, index) => {
+  const handleOtpChange = ({ target }) => {
     const { value } = target;
     const newOtp = [...otp];
-    newOtp[index] = value.substring(value.length - 1, value.length);
+    newOtp[currentOTPIndex] = value.substring(value.length - 1, value.length);
 
-    if (!value) focusPrevInputField(index);
-    else focusNextInputField(index);
+    if (!value) focusPrevInputField(currentOTPIndex);
+    else focusNextInputField(currentOTPIndex);
     setOtp([...newOtp]);
+  };
+
+  const handleKeyDown = ({ key }, index) => {
+    currentOTPIndex = index;
+    if (key === "Backspace") {
+      focusPrevInputField(currentOTPIndex);
+    }
   };
 
   useEffect(() => {
@@ -55,7 +63,8 @@ export default function EmailVerification() {
                   key={index}
                   type="number"
                   value={otp[index] || ""}
-                  onChange={(e) => handleOtpChange(e, index)}
+                  onChange={handleOtpChange}
+                  onKeyDown={(e) => handleKeyDown(e, index)}
                   className="w-12 h-12 border-2 border-dark-subtle focus:border-white rounded bg-transparent outline-none text-center text-white font-semibold text-xl spin-button-none"
                 />
               );
