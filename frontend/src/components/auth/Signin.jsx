6c7a9f46dc7abcca9a44bb5cd1719f3677e5ea42
onurtaskirancom <taskiranonur9@gmail.com>
2023-01-18 10:10:48 +0300

    Different Color for Light & Dark Mode

--- frontend/src/components/auth/Signin.jsx
+++ frontend/src/components/auth/Signin.jsx
@@ -8,9 +8,9 @@ import Title from "../form/Title";
 
 export default function Signin() {
   return (
-    <div className="fixed inset-0 dark:bg-primary -z-10 flex justify-center items-center">
+    <div className="fixed inset-0 dark:bg-primary bg-white -z-10 flex justify-center items-center">
       <Container>
-        <form className="dark:bg-secondary rounded p-6 w-72 space-y-6">
+        <form className="dark:bg-secondary bg-white drop-shadow-lg rounded p-6 w-72 space-y-6">
           <Title>Sign in</Title>
           <FormInput label="Email" placeholder="john@email.com" name="email" />
           <FormInput label="Password" placeholder="********" name="password" />
