fdacacd1f16b48043e4da7a96d254032ce23fe10
onurtaskirancom <taskiranonur9@gmail.com>
2023-01-20 09:27:51 +0300

    Signup State

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { commonModalClasses } from "../../utils/theme";
 import Container from "../Container";
 import CustomLink from "../CustomLink";
@@ -8,14 +8,51 @@ import Submit from "../form/Submit";
 import Title from "../form/Title";
 
 export default function Signup() {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userInfo);
+  };
+
+  const { name, email, password } = userInfo;
+
   return (
     <FormContainer>
       <Container>
-        <form className={commonModalClasses + " w-72"}>
+        <form onSubmit={handleSubmit} className={commonModalClasses + " w-72"}>
           <Title>Sign up</Title>
-          <FormInput label="Name" placeholder="John Doe" name="name" />
-          <FormInput label="Email" placeholder="john@email.com" name="email" />
-          <FormInput label="Password" placeholder="********" name="password" />
+          <FormInput
+            value={name}
+            onChange={handleChange}
+            label="Name"
+            placeholder="John Doe"
+            name="name"
+          />
+          <FormInput
+            value={email}
+            onChange={handleChange}
+            label="Email"
+            placeholder="john@email.com"
+            name="email"
+          />
+          <FormInput
+            value={password}
+            onChange={handleChange}
+            label="Password"
+            placeholder="********"
+            name="password"
+            type="password"
+          />
           <Submit value="Sign up" />
 
           <div className="flex justify-between">
