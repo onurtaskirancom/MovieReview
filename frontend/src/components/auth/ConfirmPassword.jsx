a1d1cdb2f52b0b05c71be67dc263a5a0e31414b7
onurtaskirancom <taskiranonur9@gmail.com>
2023-02-13 17:45:39 +0300

    Handling Invalid and Valid Token

--- frontend/src/components/auth/ConfirmPassword.jsx
+++ frontend/src/components/auth/ConfirmPassword.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { ImSpinner3 } from "react-icons/im";
 import { commonModalClasses } from "../../utils/theme";
 import Container from "../Container";
@@ -7,14 +7,39 @@ import FormContainer from "../form/FormContainer";
 import FormInput from "../form/FormInput";
 import Submit from "../form/Submit";
 import Title from "../form/Title";
+import { verifyPasswordResetToken } from "../../api/auth";
+import { useNotification } from "../../hooks";
 
 export default function ConfirmPassword() {
-  const [isVerifying, setIsVerifying] = useState(true);
+  const [isVerifying, setIsVerifying] = useState(false);
+  const [isValid, setIsValid] = useState(false);
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
   const id = searchParams.get("id");
 
+  const { updateNotification } = useNotification();
+  const navigate = useNavigate();
+
   // isValid, !isValid
+  useEffect(() => {
+    isValidToken();
+  }, []);
+
+  const isValidToken = async () => {
+    const { error, valid } = await verifyPasswordResetToken(token, id);
+    setIsVerifying(false);
+    if (error) {
+      navigate("/auth/reset-password", { replace: true });
+      return updateNotification("error", error);
+    }
+
+    if (!valid) {
+      setIsValid(false);
+      return navigate("/auth/reset-password", { replace: true });
+    }
+
+    setIsValid(true);
+  };
 
   if (isVerifying)
     return (
@@ -26,6 +51,17 @@ export default function ConfirmPassword() {
             </h1>
             <ImSpinner3 className="animate-spin text-4xl dark:text-white text-primary" />
           </div>
+        </Container>
+      </FormContainer>
+    );
+
+  if (!isValid)
+    return (
+      <FormContainer>
+        <Container>
+          <h1 className="text-4xl font-semibold dark:text-white text-primary">
+            Sorry the token is invalid!
+          </h1>
         </Container>
       </FormContainer>
     );
