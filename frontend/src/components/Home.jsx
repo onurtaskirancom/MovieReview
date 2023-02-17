faccdeb50b599f5dc373e097a4487ef60130a16d
onurtaskirancom <taskiranonur9@gmail.com>
2023-02-17 05:27:12 +0300

    Conditionally Rendering Resend Email Verification

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,15 +4,19 @@ import Container from "./Container";
 
 export default function Home() {
   const { authInfo } = useAuth();
-  console.log(authInfo);
+  const { isLoggedIn } = authInfo;
+  const isVerified = authInfo.profile?.isVerified;
+
   return (
     <Container>
-      <p className="text-lg text-center bg-blue-50 p-2">
-        It looks like you haven't verified your account,{" "}
-        <button className="text-blue-500 font-semibold hover:underline">
-          click here to verify your account.
-        </button>
-      </p>
+      {isLoggedIn && !isVerified ? (
+        <p className="text-lg text-center bg-blue-50 p-2">
+          It looks like you haven't verified your account,{" "}
+          <button className="text-blue-500 font-semibold hover:underline">
+            click here to verify your account.
+          </button>
+        </p>
+      ) : null}
     </Container>
   );
 }
