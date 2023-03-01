3d240d77288bdbd94be830825f0729768b539d4d
onurtaskirancom <taskiranonur9@gmail.com>
2023-03-01 05:02:23 +0300

    Optionally Uploading Image

--- backend/controllers/actor.js
+++ backend/controllers/actor.js
@@ -13,9 +13,20 @@ exports.createActor = async (req, res) => {
   const { file } = req;
 
   const newActor = new Actor({ name, about, gender });
-  const { secure_url, public_id } = await cloudinary.uploader.upload(file.path);
 
-  newActor.avatar = { url: secure_url, public_id };
+  if (file) {
+    const { secure_url, public_id } = await cloudinary.uploader.upload(
+      file.path
+    );
+    newActor.avatar = { url: secure_url, public_id };
+  }
   await newActor.save();
-  res.status(201).json(newActor);
+  res.status(201).json({
+    id: newActor._id,
+    name,
+    about,
+    gender,
+    avatar: newActor.avatar?.url,
+  });
 };
+  
