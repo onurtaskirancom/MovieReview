05500fe7b3381263b011ac1acd23a27e5a2fbeae
onurtaskirancom <taskiranonur9@gmail.com>
2023-03-13 16:09:26 +0300

    Trailer Upload Route

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,23 @@
-require('express-async-errors')
-const express = require("express");
-require("express-async-errors");
-const morgan = require("morgan");
-const { errorHandler } = require("./middlewares/error");
+require('express-async-errors');
+const express = require('express');
+const morgan = require('morgan');
+const { errorHandler } = require('./middlewares/error');
 const cors = require('cors');
-
-require('dotenv').config()
-require('./db')
-const userRouter = require("./routes/user");
-const actorRouter = require("./routes/actor");
-const { handleNotFound } = require("./utils/helper");
+require('dotenv').config();
+require('./db');
+const userRouter = require('./routes/user');
+const actorRouter = require('./routes/actor');
+const movieRouter = require('./routes/movie');
+const { handleNotFound } = require('./utils/helper');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
-app.use("/api/user", userRouter);
-app.use("/api/actor", actorRouter);
-app.use("/*", handleNotFound);
+app.use(morgan('dev'));
+app.use('/api/user', userRouter);
+app.use('/api/actor', actorRouter);
+app.use('/api/movie', movieRouter);
+app.use('/*', handleNotFound);
 
 app.use(errorHandler);
 
@@ -33,5 +33,5 @@ app.use(errorHandler);
 //   });
 
 app.listen(8000, () => {
-  console.log("the port is listening on port 8000");
+  console.log('the port is listening on port 8000');
 });
