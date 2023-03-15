370825b26792a6520045c49f4d488dcebbd36519
onurtaskirancom <taskiranonur9@gmail.com>
2023-03-15 16:03:40 +0300

    The First Step Towards Uploading a Movie

--- backend/routes/movie.js
+++ backend/routes/movie.js
@@ -1,15 +1,22 @@
-const express = require("express");
-const { uploadTrailer } = require("../controllers/movie");
-const { isAuth, isAdmin } = require("../middlewares/auth");
-const { uploadVideo } = require("../middlewares/multer");
+const express = require('express');
+const { uploadTrailer, createMovie } = require('../controllers/movie');
+const { isAuth, isAdmin } = require('../middlewares/auth');
+const { uploadVideo, uploadImage } = require('../middlewares/multer');
 const router = express.Router();
 
 router.post(
-  "/upload-trailer",
+  '/upload-trailer',
   isAuth,
   isAdmin,
-  uploadVideo.single("video"),
+  uploadVideo.single('video'),
   uploadTrailer
+);
+router.post(
+  '/create',
+  isAuth,
+  isAdmin,
+  uploadImage.single('poster'),
+  createMovie
 );
 
 module.exports = router;
