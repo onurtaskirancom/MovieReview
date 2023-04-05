f502d156c3665148a170fb59d09d93375967c176
onurtaskirancom <taskiranonur9@gmail.com>
2023-04-05 05:38:46 +0300

    Handling Logout from Admin Panel

--- frontend/src/components/admin/Navbar.jsx
+++ frontend/src/components/admin/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import { AiOutlineHome } from "react-icons/ai";
-import { BiMoviePlay } from "react-icons/bi";
-import { FaUserNinja } from "react-icons/fa";
-import { FiLogOut } from "react-icons/fi";
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { AiOutlineHome } from 'react-icons/ai';
+import { BiMoviePlay } from 'react-icons/bi';
+import { FaUserNinja } from 'react-icons/fa';
+import { FiLogOut } from 'react-icons/fi';
+import { useAuth } from '../../hooks';
 
 export default function Navbar() {
+  const { handleLogout } = useAuth();
   return (
     <nav className="w-48 min-h-screen bg-secondary border-r border-gray-300">
       <div className="flex flex-col justify-between pl-5 h-screen sticky top-0">
@@ -36,7 +38,10 @@ export default function Navbar() {
         </ul>
         <div className="flex flex-col items-start pb-5">
           <span className="font-semibold text-white text-xl">Admin</span>
-          <button className="flex items-center text-dark-subtle text-sm hover:text-white transition space-x-1">
+          <button
+            onClick={handleLogout}
+            className="flex items-center text-dark-subtle text-sm hover:text-white transition space-x-1"
+          >
             <FiLogOut />
             <span>Log out</span>
           </button>
@@ -48,11 +53,11 @@ export default function Navbar() {
 
 const NavItem = ({ children, to }) => {
   const commonClasses =
-    " flex items-center text-lg space-x-2 p-2 hover:opacity-80";
+    ' flex items-center text-lg space-x-2 p-2 hover:opacity-80';
   return (
     <NavLink
       className={({ isActive }) =>
-        (isActive ? "text-white" : "text-gray-400") + commonClasses
+        (isActive ? 'text-white' : 'text-gray-400') + commonClasses
       }
       to={to}
     >
