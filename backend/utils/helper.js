11c99537a23fb09debe57b3f2234ad959b2fa7a5
onurtaskirancom <taskiranonur9@gmail.com>
2023-04-12 04:22:20 +0300

    error fix

--- backend/utils/helper.js
+++ backend/utils/helper.js
@@ -1,5 +1,5 @@
-const crypto = require('crypto');
-const cloudinary = require('../cloud');
+const crypto = require("crypto");
+const cloudinary = require("../cloud");
 
 exports.sendError = (res, error, statusCode = 401) =>
   res.status(statusCode).json({ error });
@@ -8,7 +8,7 @@ exports.generateRandomByte = () => {
   return new Promise((resolve, reject) => {
     crypto.randomBytes(30, (err, buff) => {
       if (err) reject(err);
-      const buffString = buff.toString('hex');
+      const buffString = buff.toString("hex");
 
       resolve(buffString);
     });
@@ -16,13 +16,13 @@ exports.generateRandomByte = () => {
 };
 
 exports.handleNotFound = (req, res) => {
-  this.sendError(res, 'Not found', 404);
+  this.sendError(res, "Not found", 404);
 };
 
 exports.uploadImageToCloud = async (file) => {
   const { secure_url: url, public_id } = await cloudinary.uploader.upload(
     file,
-    { gravity: 'face', height: 500, width: 500, crop: 'thumb' }
+    { gravity: "face", height: 500, width: 500, crop: "thumb" }
   );
 
   return { url, public_id };
@@ -37,4 +37,15 @@ exports.formatActor = (actor) => {
     gender,
     avatar: avatar?.url,
   };
+};
+
+exports.parseData = (req, res, next) => {
+  const { trailer, cast, genres, tags, writers } = req.body;
+  if (trailer) req.body.trailer = JSON.parse(trailer);
+  if (cast) req.body.cast = JSON.parse(cast);
+  if (genres) req.body.genres = JSON.parse(genres);
+  if (tags) req.body.tags = JSON.parse(tags);
+  if (writers) req.body.writers = JSON.parse(writers);
+
+  next();
 };
