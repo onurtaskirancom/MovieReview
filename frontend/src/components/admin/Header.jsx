ea1f9a02f07ec95cabf07e890bc78c407f1ef381
onurtaskirancom <taskiranonur9@gmail.com>
2023-04-15 02:55:02 +0300

    Theme Toggling Button Inside Admin Panel

--- frontend/src/components/admin/Header.jsx
+++ frontend/src/components/admin/Header.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
-import { AiOutlinePlus } from "react-icons/ai";
+import React, { useEffect, useRef, useState } from 'react';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { BsFillSunFill } from 'react-icons/bs';
+import { useTheme } from '../../hooks';
 
 export default function Header() {
   const [showOptions, setShowOptions] = useState(false);
+  const { toggleTheme } = useTheme();
 
   return (
     <div className="flex items-center justify-between relative">
@@ -11,25 +14,35 @@ export default function Header() {
         className="border-2 dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary dark:text-white transition bg-transparent rounded text-lg p-1 outline-none"
         placeholder="Search Movies..."
       />
-      <button
-        onClick={() => setShowOptions(true)}
-        className="flex items-center space-x-2 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 rounded text-lg px-3 py-1"
-      >
-        <span>Create</span>
-        <AiOutlinePlus />
-      </button>
 
-      <CreateOptions
-        visible={showOptions}
-        onClose={() => setShowOptions(false)}
-      />
+      <div className="flex items-center space-x-3">
+        <button
+          onClick={toggleTheme}
+          className="dark:text-white text-light-subtle"
+        >
+          <BsFillSunFill size={24} />
+        </button>
+
+        <button
+          onClick={() => setShowOptions(true)}
+          className="flex items-center space-x-2 dark:border-dark-subtle border-light-subtle dark:text-dark-subtle text-light-subtle hover:opacity-80 transition font-semibold border-2 rounded text-lg px-3 py-1"
+        >
+          <span>Create</span>
+          <AiOutlinePlus />
+        </button>
+
+        <CreateOptions
+          visible={showOptions}
+          onClose={() => setShowOptions(false)}
+        />
+      </div>
     </div>
   );
 }
 
 const CreateOptions = ({ visible, onClose }) => {
   const container = useRef();
-  const containerID = "options-container";
+  const containerID = 'options-container';
 
   useEffect(() => {
     const handleClose = (e) => {
@@ -44,14 +57,14 @@ const CreateOptions = ({ visible, onClose }) => {
 
       // New Update
       if (container.current) {
-        if (!container.current.classList.contains("animate-scale"))
-          container.current.classList.add("animate-scale-reverse");
+        if (!container.current.classList.contains('animate-scale'))
+          container.current.classList.add('animate-scale-reverse');
       }
     };
 
-    document.addEventListener("click", handleClose);
+    document.addEventListener('click', handleClose);
     return () => {
-      document.removeEventListener("click", handleClose);
+      document.removeEventListener('click', handleClose);
     };
   }, [visible]);
 
@@ -63,8 +76,8 @@ const CreateOptions = ({ visible, onClose }) => {
       ref={container}
       className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded animate-scale"
       onAnimationEnd={(e) => {
-        if (e.target.classList.contains("animate-scale-reverse")) onClose();
-        e.target.classList.remove("animate-scale");
+        if (e.target.classList.contains('animate-scale-reverse')) onClose();
+        e.target.classList.remove('animate-scale');
       }}
     >
       <Option>Add Movie</Option>
