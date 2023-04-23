749080e1ab061cac7038577e41e71f7ac10fb2af
onurtaskirancom <taskiranonur9@gmail.com>
2023-04-23 05:07:32 +0300

    Component to Display Upload Progress

--- frontend/src/components/admin/MovieUpload.jsx
+++ frontend/src/components/admin/MovieUpload.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import { FileUploader } from "react-drag-drop-files";
-import { AiOutlineCloudUpload } from "react-icons/ai";
-import { uploadTrailer } from "../../api/movie";
-import { useNotification } from "../../hooks";
+import React, { useState } from 'react';
+import { FileUploader } from 'react-drag-drop-files';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { uploadTrailer } from '../../api/movie';
+import { useNotification } from '../../hooks';
 
 export default function MovieUpload() {
   const [videoSelected, setVideoSelected] = useState(false);
   const { updateNotification } = useNotification();
 
   const handleTypeError = (error) => {
-    updateNotification("error", error);
+    updateNotification('error', error);
   };
 
   const handleChange = async (file) => {
     const formData = new FormData();
-    formData.append("video", file);
+    formData.append('video', file);
 
     const res = await uploadTrailer(formData);
     console.log(res);
@@ -23,6 +23,20 @@ export default function MovieUpload() {
   return (
     <div className="fixed inset-0 dark:bg-white dark:bg-opacity-50 bg-primary bg-opacity-50 backdrop-blur-sm flex items-center justify-center">
       <div className="dark:bg-primary bg-white rounded w-[45rem] h-[40rem] overflow-auto">
+        <div className="p-2">
+          <div className="dark:bg-secondary bg-white drop-shadow-lg rounded p-3">
+            <div className="relative h-3 dark:bg-dark-subtle bg-light-subtle overflow-hidden">
+              <div
+                style={{ width: '80%' }}
+                className="h-full absolute left-0 dark:bg-white bg-secondary"
+              />
+            </div>
+            <p className="font-semibold dark:text-dark-subtle text-light-subtle animate-pulse mt-1">
+              Upload progress 80%
+            </p>
+          </div>
+        </div>
+
         <TrailerSelector
           visible={!videoSelected}
           onTypeError={handleTypeError}
@@ -41,7 +55,7 @@ const TrailerSelector = ({ visible, handleChange, onTypeError }) => {
       <FileUploader
         handleChange={handleChange}
         onTypeError={onTypeError}
-        types={["mp4", "avi"]}
+        types={['mp4', 'avi']}
       >
         <div className="w-48 h-48 border border-dashed dark:border-dark-subtle border-light-subtle rounded-full flex flex-col items-center justify-center dark:text-dark-subtle text-secondary cursor-pointer">
           <AiOutlineCloudUpload size={80} />
