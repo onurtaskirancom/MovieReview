2dc12ed008d1d8edb3ede9112f46b68a70588fe8
onurtaskirancom <taskiranonur9@gmail.com>
2023-05-01 13:19:16 +0300

    Styling Tag Component

--- frontend/src/components/TagsInput.jsx
+++ frontend/src/components/TagsInput.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 
 export default function TagsInput() {
   return (
     <div>
       <div className="border-2 bg-transparent dark:border-dark-subtle border-light-subtle px-2 h-10 rounded w-full text-white flex items-center space-x-2">
-        <span>Tag one</span>
-        <span>Tag two</span>
-        <span>Tag three</span>
-        <input className="h-full flex-grow bg-transparent outline-none dark:text-white" />
+        <Tag>Tag two</Tag>
+        <Tag>Tag three</Tag>
+        <input
+          type="text"
+          className="h-full flex-grow bg-transparent outline-none dark:text-white"
+          placeholder="Tag one, Tag two"
+        />
       </div>
     </div>
   );
 }
+
+const Tag = ({ children, onClick }) => {
+  return (
+    <span className="dark:bg-white bg-primary dark:text-primary text-white flex items-center text-sm px-1">
+      {children}
+      <button onClick={onClick} type="button">
+        <AiOutlineClose size={12} />
+      </button>
+    </span>
+  );
+};
