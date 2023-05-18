4fceaad3cf58c483da377f16b360190d279b2335
onurtaskirancom <taskiranonur9@gmail.com>
2023-05-18 03:10:33 +0300

    Refactor Live Search Component Part 2

--- frontend/src/components/LiveSearch.jsx
+++ frontend/src/components/LiveSearch.jsx
@@ -1,46 +1,17 @@
 import React, { useEffect, useRef, useState, forwardRef } from 'react';
 import { commonInputClasses } from '../utils/theme';
 
-export const results = [
-  {
-    id: '1',
-    avatar:
-      'https://images.unsplash.com/photo-1643713303351-01f540054fd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80',
-    name: 'John Doe',
-  },
-  {
-    id: '2',
-    avatar:
-      'https://images.unsplash.com/photo-1643883135036-98ec2d9e50a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80',
-    name: 'Chandri Anggara',
-  },
-  {
-    id: '3',
-    avatar:
-      'https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80',
-    name: 'Amin RK',
-  },
-  {
-    id: '4',
-    avatar:
-      'https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80',
-    name: 'Edward Howell',
-  },
-  {
-    id: '5',
-    avatar:
-      'https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80',
-    name: 'Amin RK',
-  },
-  {
-    id: '6',
-    avatar:
-      'https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80',
-    name: 'Edward Howell',
-  },
-];
-
-export default function LiveSearch() {
+export default function LiveSearch({
+  value = '',
+  placeholder = '',
+  results = [],
+  resultContainerStyle,
+  selectedResultStyle,
+  inputStyle,
+  renderItem = null,
+  onChange = null,
+  onSelect = null,
+}) {
   const [displaySearch, setDisplaySearch] = useState(false);
   const [focusedIndex, setFocusedIndex] = useState(-1);
 
@@ -58,7 +29,7 @@ export default function LiveSearch() {
   };
 
   const handleSelection = (selectedItem) => {
-    console.log(selectedItem);
+    onSelect(selectedItem);
   };
 
   const handleKeyDown = ({ key }) => {
@@ -79,6 +50,12 @@ export default function LiveSearch() {
     setFocusedIndex(nextCount);
   };
 
+  const getInputStyle = () => {
+    return inputStyle
+      ? inputStyle
+      : commonInputClasses + ' border-2 rounded p-1 text-lg';
+  };
+
   return (
     <div
       tabIndex={1}
@@ -88,9 +65,11 @@ export default function LiveSearch() {
     >
       <input
         type="text"
-        className={commonInputClasses + ' border-2 rounded p-1 text-lg'}
-        placeholder="Search profile"
+        className={getInputStyle()}
+        placeholder={placeholder}
         onFocus={handleOnFocus}
+        value={value}
+        onChange={onChange}
         // onBlur={handleOnBlur}
         // onKeyDown={handleKeyDown}
       />
@@ -99,6 +78,9 @@ export default function LiveSearch() {
         visible={displaySearch}
         focusedIndex={focusedIndex}
         onSelect={handleSelection}
+        renderItem={renderItem}
+        resultContainerStyle={resultContainerStyle}
+        selectedResultStyle={selectedResultStyle}
       />
     </div>
   );
