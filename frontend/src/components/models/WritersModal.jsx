0555230cc68b80c20ae92308f79ed5d9bae79939
onurtaskirancom <taskiranonur9@gmail.com>
2023-05-27 05:54:17 +0300

    Adding Styles to Writers Profile

--- frontend/src/components/models/WritersModal.jsx
+++ frontend/src/components/models/WritersModal.jsx
@@ -1,17 +1,29 @@
-import React from "react";
-import ModalContainer from "./ModalContainer";
+import React from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
+import ModalContainer from './ModalContainer';
 
 export default function WritersModal({ profiles = [], visible, onClose }) {
   return (
     <ModalContainer onClose={onClose} visible={visible}>
-      {profiles.map(({ id, name, avatar }) => {
-        return (
-          <div key={id}>
-            <img src={avatar} alt={name} />
-            <p>{name}</p>
-          </div>
-        );
-      })}
+      <div className="space-y-2">
+        {profiles.map(({ id, name, avatar }) => {
+          return (
+            <div key={id} className="flex space-x-3">
+              <img
+                className="w-16 h-16 aspect-square rounded object-cover"
+                src={avatar}
+                alt={name}
+              />
+              <p className="w-full font-semibold dark:text-white text-primary">
+                {name}
+              </p>
+              <button className="dark:text-white text-primary hover:opacity-80 transition p-2">
+                <AiOutlineClose />
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </ModalContainer>
   );
 }
