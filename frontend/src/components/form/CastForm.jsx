def51ab1f4b042846a5ef885f2632a8f7a4767b6
onurtaskirancom <taskiranonur9@gmail.com>
2023-05-31 17:39:12 +0300

    Managing Cast Form State

--- frontend/src/components/form/CastForm.jsx
+++ frontend/src/components/form/CastForm.jsx
@@ -1,17 +1,35 @@
-import React, { useState } from "react";
-import { commonInputClasses } from "../../utils/theme";
-import LiveSearch from "../LiveSearch";
+import React, { useState } from 'react';
+import { commonInputClasses } from '../../utils/theme';
+import LiveSearch from '../LiveSearch';
+import { renderItem, results } from '../admin/MovieForm';
 
 // const cast = [{ actor: id, roleAs: "", leadActor: true }];
 const defaultCastInfo = {
   profile: {},
-  roleAs: "",
+  roleAs: '',
   leadActor: false,
 };
 export default function CastForm() {
   const [castInfo, setCastInfo] = useState({ ...defaultCastInfo });
 
-  const { leadActor } = castInfo;
+  const handleOnChange = ({ target }) => {
+    const { checked, name, value } = target;
+
+    if (name === 'leadActor')
+      return setCastInfo({ ...castInfo, leadActor: checked });
+
+    setCastInfo({ ...castInfo, [name]: value });
+  };
+
+  const handleProfileSelect = (profile) => {
+    setCastInfo({ ...castInfo, profile });
+  };
+
+  const handleSubmit = () => {
+    console.log(castInfo);
+  };
+
+  const { leadActor, profile, roleAs } = castInfo;
   return (
     <div className="flex items-center space-x-2">
       <input
@@ -19,8 +37,15 @@ export default function CastForm() {
         name="leadActor"
         className="w-4 h-4"
         checked={leadActor}
+        onChange={handleOnChange}
       />
-      <LiveSearch placeholder="Search profile" />
+      <LiveSearch
+        placeholder="Search profile"
+        value={profile.name}
+        results={results}
+        onSelect={handleProfileSelect}
+        renderItem={renderItem}
+      />
       <span className="dark:text-dark-subtle text-light-subtle font-semibold">
         as
       </span>
@@ -28,12 +53,18 @@ export default function CastForm() {
       <div className="flex-grow">
         <input
           type="text"
-          className={commonInputClasses + " rounded p-1 text-lg border-2"}
+          className={commonInputClasses + ' rounded p-1 text-lg border-2'}
           placeholder="Role as"
+          name="roleAs"
+          value={roleAs}
+          onChange={handleOnChange}
         />
       </div>
 
-      <button className="bg-secondary dark:bg-white dark:text-primary text-white px-1 rounded">
+      <button
+        onClick={handleSubmit}
+        className="bg-secondary dark:bg-white dark:text-primary text-white px-1 rounded"
+      >
         Add
       </button>
     </div>
