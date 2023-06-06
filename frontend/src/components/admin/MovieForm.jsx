e09a4f7dcec4f02fb0f3309f583bde06e097eed3
onurtaskirancom <taskiranonur9@gmail.com>
2023-06-06 06:49:18 +0300

    Little Fix inside Tags Input Component

--- frontend/src/components/admin/MovieForm.jsx
+++ frontend/src/components/admin/MovieForm.jsx
@@ -146,7 +146,7 @@ export default function MovieForm() {
     setMovieInfo({ ...movieInfo, cast: [...newCast] });
   };
 
-  const { title, storyLine, director, writers, cast } = movieInfo;
+  const { title, storyLine, director, writers, cast, tags } = movieInfo;
 
   return (
     <>
@@ -181,7 +181,7 @@ export default function MovieForm() {
 
           <div>
             <Label htmlFor="tags">Tags</Label>
-            <TagsInput name="tags" onChange={updateTags} />
+            <TagsInput value={tags} name="tags" onChange={updateTags} />
           </div>
 
           <div className="">
