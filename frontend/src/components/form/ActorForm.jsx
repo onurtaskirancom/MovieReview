8917db326ffdd4b5ed08fb85b8420c1b62679e97
onurtaskirancom <taskiranonur9@gmail.com>
2023-06-26 15:56:17 +0300

    Handling Actor Form State

--- frontend/src/components/form/ActorForm.jsx
+++ frontend/src/components/form/ActorForm.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { commonInputClasses } from '../../utils/theme';
 import PosterSelector from '../PosterSelector';
 
+const defaultActorInfo = {
+  name: '',
+  about: '',
+  avatar: null,
+};
+
 export default function ActorForm({ title, btnTitle }) {
+  const [actorInfo, setActorInfo] = useState({ ...defaultActorInfo });
+  const [selectedAvatarForUI, setSelectedAvatarForUI] = useState('');
+
+  const updatePosterForUI = (file) => {
+    const url = URL.createObjectURL(file);
+    setSelectedAvatarForUI(url);
+  };
+
+  const handleChange = ({ target }) => {
+    const { value, files, name } = target;
+    if (name === 'avatar') {
+      const file = files[0];
+      updatePosterForUI(file);
+      return setActorInfo({ ...actorInfo, avatar: file });
+    }
+
+    setActorInfo({ ...actorInfo, [name]: value });
+  };
+
+  const { name, about } = actorInfo;
   return (
     <div className="dark:bg-primary bg-white p-3 w-[35rem] rounded">
       <div className="flex justify-between items-center mb-3">
@@ -10,7 +36,7 @@ export default function ActorForm({ title, btnTitle }) {
           {title}
         </h1>
         <button
-          className="px-3 py-1 bg-primary text-white dark:bg-white dark:text-primary hover:opacity-80 transition rounded"
+          className="h-8 w-24 bg-primary text-white dark:bg-white dark:text-primary hover:opacity-80 transition rounded flex items-center justify-center"
           type="submit"
         >
           {btnTitle}
@@ -18,14 +44,25 @@ export default function ActorForm({ title, btnTitle }) {
       </div>
 
       <form className="flex space-x-2">
-        <PosterSelector className="w-36 h-36 aspect-square object-cover" />
+        <PosterSelector
+          selectedPoster={selectedAvatarForUI}
+          className="w-36 h-36 aspect-square object-cover"
+          name="avatar"
+          onChange={handleChange}
+        />
         <div className="flex-grow flex flex-col space-y-2">
           <input
             placeholder="Enter name"
             type="text"
             className={commonInputClasses + ' border-b-2'}
+            name="name"
+            value={name}
+            onChange={handleChange}
           />
           <textarea
+            name="about"
+            value={about}
+            onChange={handleChange}
             placeholder="About"
             className={commonInputClasses + ' border-b-2 resize-none h-full'}
           ></textarea>
