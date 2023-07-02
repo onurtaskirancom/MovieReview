055413eab65d19e3840ef24449f782903c3f3a34
onurtaskirancom <taskiranonur9@gmail.com>
2023-07-02 14:24:16 +0300

    Closing Modal After Success Response

--- frontend/src/components/models/ActorUpload.jsx
+++ frontend/src/components/models/ActorUpload.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { createActor } from '../../api/actor';
+import { useNotification } from '../../hooks';
 import ActorForm from '../form/ActorForm';
 import ModalContainer from './ModalContainer';
 
 export default function ActorUpload({ visible, onClose }) {
+  const { updateNotification } = useNotification();
+
   const handleSubmit = async (data) => {
-    const res = await createActor(data);
-    console.log(res);
+    const { error, actor } = await createActor(data);
+    if (error) return updateNotification('error', error);
+
+    updateNotification('success', 'Actor created successfully.');
+    onClose();
   };
 
   return (
