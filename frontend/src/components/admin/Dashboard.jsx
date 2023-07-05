3141d6794eb405b6971de94c544e5598fe720457
onurtaskirancom <taskiranonur9@gmail.com>
2023-07-05 07:53:44 +0300

    Writing our own debounce function

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import MovieUpload from './MovieUpload';
 
+let timeoutId;
+const debounce = (func, delay) => {
+  return (...args) => {
+    if (timeoutId) clearTimeout();
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
+
 export default function Dashboard() {
-  return <MovieUpload />;
+  const search = (value) => {
+    console.log(value);
+  };
+  const debounceSearch = debounce(search, 500);
+
+  const handleChange = ({ target }) => {
+    debounceSearch(target.value);
+  };
+
+  return (
+    <div className="p-14">
+      <input
+        onChange={handleChange}
+        type="text"
+        className="border border-gray-500"
+      />
+    </div>
+  );
 }
