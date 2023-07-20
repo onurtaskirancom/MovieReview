f89b24a4442381ba27a645fe9c633c808690c2e3
onurtaskirancom <taskiranonur9@gmail.com>
2023-07-20 05:37:18 +0300

    Creating Info Container

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -2,5 +2,12 @@ import React from 'react';
 import MovieUpload from './MovieUpload';
 
 export default function Dashboard() {
-  return null;
+  return (
+    <div className="bg-white shadow dark:shadow dark:bg-secondary p-5 rounded">
+      <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
+        Total Uploads
+      </h1>
+      <p className="text-xl text-primary dark:text-white">100</p>
+    </div>
+  );
 }
