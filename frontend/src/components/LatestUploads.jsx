4159d37a4c71ac0dcec10a0bf9802faf76ae5d1d
onurtaskirancom <taskiranonur9@gmail.com>
2023-08-30 03:05:16 +0300

    Fetching Latest Uploads For Admin

--- frontend/src/components/LatestUploads.jsx
+++ frontend/src/components/LatestUploads.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getMovies } from '../api/movie';
+import { useNotification } from '../hooks';
 import MovieListItem from './MovieListItem';
 
+const pageNo = 0;
+const limit = 5;
+
 export default function LatestUploads() {
+  const [movies, setMovies] = useState([]);
+  const { updateNotification } = useNotification();
+
+  const fetchLatestUploads = async () => {
+    const { error, movies } = await getMovies(pageNo, limit);
+    if (error) return updateNotification(error);
+
+    setMovies([...movies]);
+  };
+
+  useEffect(() => {
+    fetchLatestUploads();
+  }, []);
+
   return (
     <div className="bg-white shadow dark:shadow dark:bg-secondary p-5 rounded col-span-2">
       <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
         Recent Uploads
       </h1>
 
-      <MovieListItem
-        movie={{
-          poster:
-            'https://images.unsplash.com/photo-1656119651670-ec9610795a3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
-          title: 'Lorem ipsum dolor sit amet.',
-          status: 'public',
-          genres: ['Action', 'Comedy'],
-        }}
-      />
+      <div className="space-y-3">
+        {movies.map((movie) => {
+          return <MovieListItem key={movie.id} movie={movie} />;
+        })}
+      </div>
     </div>
   );
 }
