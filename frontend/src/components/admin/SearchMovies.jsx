cf109995237bd954d34c60411ab968e687185741
onurtaskirancom <taskiranonur9@gmail.com>
2023-09-05 05:51:11 +0300

    Grabbing query from the URL

--- frontend/src/components/admin/SearchMovies.jsx
+++ frontend/src/components/admin/SearchMovies.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
 export default function SearchMovies() {
-  return <div>SearchMovies</div>;
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("title");
+
+  const searchMovies = (val) => {
+    // call the backend api
+    console.log(val);
+  };
+
+  useEffect(() => {
+    if (query.trim()) searchMovies(query);
+  }, [query]);
+
+  return <div>SearchMovie</div>;
 }
