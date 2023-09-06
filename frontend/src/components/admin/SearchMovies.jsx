dbf679e7a86714786d0f329fe10ea9e42e16904a
onurtaskirancom <taskiranonur9@gmail.com>
2023-09-06 07:53:18 +0300

    Searching Movies

--- frontend/src/components/admin/SearchMovies.jsx
+++ frontend/src/components/admin/SearchMovies.jsx
@@ -1,18 +1,33 @@
-import React, { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { searchMovieForAdmin } from '../../api/movie';
+import { useNotification } from '../../hooks';
+import MovieListItem from '../MovieListItem';
 
 export default function SearchMovies() {
-  const [searchParams] = useSearchParams();
-  const query = searchParams.get("title");
+  const [movies, setMovies] = useState([]);
 
-  const searchMovies = (val) => {
-    // call the backend api
-    console.log(val);
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get('title');
+
+  const { updateNotification } = useNotification();
+
+  const searchMovies = async (val) => {
+    const { error, results } = await searchMovieForAdmin(val);
+    if (error) return updateNotification('error', error);
+
+    setMovies([...results]);
   };
 
   useEffect(() => {
     if (query.trim()) searchMovies(query);
   }, [query]);
 
-  return <div>SearchMovie</div>;
+  return (
+    <div className="p-5 space-y-3">
+      {movies.map((movie) => {
+        return <MovieListItem movie={movie} key={movie.id} />;
+      })}
+    </div>
+  );
 }
