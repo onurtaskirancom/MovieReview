e39996fd5a983e99188448cf91ec899787b4dead
onurtaskirancom <taskiranonur9@gmail.com>
2023-09-14 11:19:58 +0300

    Adding New Review

--- backend/controllers/review.js
+++ backend/controllers/review.js
@@ -1,1 +1,39 @@
-exports.addReview = (req, res) => {};
+const { isValidObjectId } = require('mongoose');
+const Movie = require('../models/movie');
+const Review = require('../models/review');
+const { sendError } = require('../utils/helper');
+
+exports.addReview = async (req, res) => {
+  const { movieId } = req.params;
+  const { content, rating } = req.body;
+  const userId = req.user._id;
+
+  if (!isValidObjectId(movieId)) return sendError(res, 'Invalid Movie!');
+
+  const movie = await Movie.findOne({ _id: movieId, status: 'public' });
+  if (!movie) return sendError(res, 'Movie not found!', 404);
+
+  const isAlreadyReviewed = await Review.findOne({
+    owner: userId,
+    parentMovie: movie._id,
+  });
+  if (isAlreadyReviewed)
+    return sendError(res, 'Invalid request, review is already their!');
+
+  // create and update review.
+  const newReview = new Review({
+    owner: userId,
+    parentMovie: movie._id,
+    content,
+    rating,
+  });
+
+  // updating review for movie.
+  movie.reviews.push(newReview._id);
+  await movie.save();
+
+  // saving new review
+  await newReview.save();
+
+  res.json({ message: 'Your review has been added.' });
+};
