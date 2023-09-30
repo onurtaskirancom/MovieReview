46b51b9a5437b0221e40dea3f024012de5a07a64
onurtaskirancom <taskiranonur9@gmail.com>
2023-09-30 14:23:42 +0300

    Fetching Top Rated Movies

--- frontend/src/components/user/TopRatedMovies.jsx
+++ frontend/src/components/user/TopRatedMovies.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getTopRatedMovies } from '../../api/movie';
+import { useNotification } from '../../hooks';
 import GridContainer from '../GridContainer';
 
 export default function TopRatedMovies() {
+  const [movies, setMovies] = useState([]);
+  const { updateNotification } = useNotification();
+
+  const fetchMovies = async () => {
+    const { error, movies } = await getTopRatedMovies();
+    if (error) return updateNotification('error', error);
+
+    setMovies([...movies]);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <GridContainer>
-      {Array(5)
-        .fill('')
-        .map((_, index) => {
-          return <div className="p-5 bg-red-200" key={index}></div>;
-        })}
+      {movies.map((_, index) => {
+        return <div className="p-5 bg-red-200" key={index}></div>;
+      })}
     </GridContainer>
   );
 }
