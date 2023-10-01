34999873a13b00127b93f8d97021ed2a0c683b62
onurtaskirancom <taskiranonur9@gmail.com>
2023-10-01 15:42:33 +0300

    Styling Movie Cards Part

--- frontend/src/components/user/TopRatedMovies.jsx
+++ frontend/src/components/user/TopRatedMovies.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { AiFillStar } from 'react-icons/ai';
 import { getTopRatedMovies } from '../../api/movie';
 import { useNotification } from '../../hooks';
 import GridContainer from '../GridContainer';
@@ -14,14 +15,44 @@ export default function TopRatedMovies() {
     setMovies([...movies]);
   };
 
+  const trimTitle = (text = '') => {
+    if (text.length <= 20) return text;
+    return text.substring(0, 20) + '..';
+  };
+
   useEffect(() => {
     fetchMovies();
   }, []);
 
   return (
     <GridContainer>
-      {movies.map((_, index) => {
-        return <div className="p-5 bg-red-200" key={index}></div>;
+      {movies.map((movie) => {
+        return (
+          <div key={movie.id}>
+            <img
+              className="aspect-video object-cover w-full"
+              src={movie.poster}
+              alt={movie.title}
+            />
+            <h1
+              className="text-lg dark:text-white text-secondary font-semibold"
+              title={movie.title}
+            >
+              {trimTitle(movie.title)}
+            </h1>
+
+            {movie.reviews?.ratingAvg ? (
+              <p className="text-highlight dark:text-highlight-dark flex items-center space-x-1">
+                <span>{movie.reviews?.ratingAvg}</span>
+                <AiFillStar />
+              </p>
+            ) : (
+              <p className="text-highlight dark:text-highlight-dark">
+                No reviews
+              </p>
+            )}
+          </div>
+        );
       })}
     </GridContainer>
   );
