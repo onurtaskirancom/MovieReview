ea0ba0380e1753b578565ce39978a6327cf14806
onurtaskirancom <taskiranonur9@gmail.com>
2023-10-02 16:31:23 +0300

    Refactoring Movie List Component

--- frontend/src/components/user/TopRatedMovies.jsx
+++ frontend/src/components/user/TopRatedMovies.jsx
@@ -3,6 +3,7 @@ import { AiFillStar } from 'react-icons/ai';
 import { getTopRatedMovies } from '../../api/movie';
 import { useNotification } from '../../hooks';
 import GridContainer from '../GridContainer';
+import MovieList from './MovieList';
 
 export default function TopRatedMovies() {
   const [movies, setMovies] = useState([]);
@@ -24,36 +25,5 @@ export default function TopRatedMovies() {
     fetchMovies();
   }, []);
 
-  return (
-    <GridContainer>
-      {movies.map((movie) => {
-        return (
-          <div key={movie.id}>
-            <img
-              className="aspect-video object-cover w-full"
-              src={movie.poster}
-              alt={movie.title}
-            />
-            <h1
-              className="text-lg dark:text-white text-secondary font-semibold"
-              title={movie.title}
-            >
-              {trimTitle(movie.title)}
-            </h1>
-
-            {movie.reviews?.ratingAvg ? (
-              <p className="text-highlight dark:text-highlight-dark flex items-center space-x-1">
-                <span>{movie.reviews?.ratingAvg}</span>
-                <AiFillStar />
-              </p>
-            ) : (
-              <p className="text-highlight dark:text-highlight-dark">
-                No reviews
-              </p>
-            )}
-          </div>
-        );
-      })}
-    </GridContainer>
-  );
+  return <MovieList movies={movies} title="Viewers choice (Movies)" />;
 }
