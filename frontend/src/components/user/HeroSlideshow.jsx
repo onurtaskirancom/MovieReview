668770ac44393ad1d45358623c165c1c5035a387
onurtaskirancom <taskiranonur9@gmail.com>
2023-10-06 17:23:46 +0300

    Slide Show Controllers

--- frontend/src/components/user/HeroSlideshow.jsx
+++ frontend/src/components/user/HeroSlideshow.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from 'react-icons/ai';
 import { getLatestUploads } from '../../api/movie';
 import { useNotification } from '../../hooks';
 
@@ -20,14 +21,32 @@ export default function HeroSlideshow() {
     fetchLatestUploads();
   }, []);
 
-  console.log(slide);
-
   return (
     <div className="w-full flex">
-      <div className="w-4/5 aspect-video">
+      {/* Slide show section */}
+      <div className="w-4/5 aspect-video relative">
         <img src={slide.poster} alt="" />
+
+        <SlideShowController />
       </div>
+
+      {/* Up Next Section */}
       <div className="w-1/5 aspect-video bg-red-300"></div>
     </div>
   );
 }
+
+const SlideShowController = ({ onNextClick, onPrevClick }) => {
+  const btnClass =
+    'bg-primary rounded border-2 text-white text-xl p-2 outline-none';
+  return (
+    <div className="absolute top-1/2 -translate-y-1/2 w-full flex items-center justify-between px-2">
+      <button onClick={onPrevClick} className={btnClass} type="button">
+        <AiOutlineDoubleLeft />
+      </button>
+      <button onClick={onNextClick} className={btnClass} type="button">
+        <AiOutlineDoubleRight />
+      </button>
+    </div>
+  );
+};
