89fab6414ea3954af3e2d3cf708b22b6a2df0cba
onurtaskirancom <taskiranonur9@gmail.com>
2023-10-21 04:22:06 +0300

    Rendering Movie Info

--- frontend/src/components/user/SingleMovie.jsx
+++ frontend/src/components/user/SingleMovie.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getSingleMovie } from '../../api/movie';
 import { useNotification } from '../../hooks';
+import Container from '../Container';
 
 export default function SingleMovie() {
   const [ready, setReady] = useState(false);
@@ -31,5 +32,26 @@ export default function SingleMovie() {
       </div>
     );
 
-  return movie.title;
+  const { id, trailer, poster, title, reviews = {} } = movie;
+
+  return (
+    <div className="dark:bg-primary bg-white min-h-screen pb-10">
+      <Container className="xl:px-0 px-2">
+        <video poster={poster} controls src={trailer}></video>
+        <div className="flex justify-between">
+          <h1 className="xl:text-4xl lg:text-3xl text-2xl  text-highlight dark:text-highlight-dark font-semibold py-3">
+            {title}
+          </h1>
+          <div className="">
+            <Link
+              className="text-highlight dark:text-highlight-dark hover:underline"
+              to={'/movie/reviews/' + id}
+            >
+              {reviews.reviewCount} Reviews
+            </Link>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
