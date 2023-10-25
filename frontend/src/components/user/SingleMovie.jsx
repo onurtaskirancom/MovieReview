19ccaa0af77d58729a0bc7a4d11fc40402f06ceb
onurtaskirancom <taskiranonur9@gmail.com>
2023-10-25 10:40:14 +0300

    Rendering Cast Profiles

--- frontend/src/components/user/SingleMovie.jsx
+++ frontend/src/components/user/SingleMovie.jsx
@@ -51,10 +51,12 @@ export default function SingleMovie() {
     storyLine,
     language,
     releseDate,
+    type,
     director = {},
     reviews = {},
     writers = [],
     cast = [],
+    genres = [],
   } = movie;
 
   return (
@@ -145,6 +147,57 @@ export default function SingleMovie() {
             <p className="text-highlight dark:text-highlight-dark">
               {convertDate(releseDate)}
             </p>
+          </div>
+
+          <div className="flex">
+            <p className="text-light-subtle dark:text-dark-subtle font-semibold mr-2">
+              Genres:
+            </p>
+            <div className="flex space-x-2">
+              {genres.map((g) => (
+                <p key={g} className="text-highlight dark:text-highlight-dark">
+                  {g}
+                </p>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex space-x-2">
+            <p className="text-light-subtle dark:text-dark-subtle font-semibold">
+              Type:
+            </p>
+            <p className="text-highlight dark:text-highlight-dark">{type}</p>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h1 className="text-light-subtle dark:text-dark-subtle font-semibold text-2xl mb-2">
+            Cast:
+          </h1>
+          <div className="grid grid-cols-10">
+            {cast.map((c) => {
+              return (
+                <div key={c.profile.id} className="flex flex-col items-center">
+                  <img
+                    className="w-24 h-24 aspect-square object-cover rounded-full"
+                    src={c.profile.avatar}
+                    alt=""
+                  />
+
+                  <p className="text-highlight dark:text-highlight-dark hover:underline cursor-pointer">
+                    {c.profile.name}
+                  </p>
+
+                  <span className="text-light-subtle dark:text-dark-subtle text-sm">
+                    as
+                  </span>
+
+                  <p className="text-light-subtle dark:text-dark-subtle">
+                    {c.roleAs}
+                  </p>
+                </div>
+              );
+            })}
           </div>
         </div>
       </Container>
