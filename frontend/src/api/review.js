5f61ec2a60394b5366e386843c89a719be0beea1
onurtaskirancom <taskiranonur9@gmail.com>
2023-11-15 01:51:01 +0300

    Fetching & Rendering Reviews

--- frontend/src/api/review.js
+++ frontend/src/api/review.js
@@ -1,14 +1,23 @@
-import { catchError, getToken } from "../utils/helper";
-import client from "./client";
+import { catchError, getToken } from '../utils/helper';
+import client from './client';
 
 export const addReview = async (movieId, reviewData) => {
   const token = getToken();
   try {
     const { data } = await client.post(`/review/add/${movieId}`, reviewData, {
       headers: {
-        authorization: "Bearer " + token,
+        authorization: 'Bearer ' + token,
       },
     });
+    return data;
+  } catch (error) {
+    return catchError(error);
+  }
+};
+
+export const getReviewByMovie = async (movieId) => {
+  try {
+    const { data } = await client(`/review/get-reviews-by-movie/${movieId}`);
     return data;
   } catch (error) {
     return catchError(error);
--- frontend/src/components/user/MovieReviews.jsx
+++ frontend/src/components/user/MovieReviews.jsx
@@ -1,8 +1,31 @@
-import React from "react";
-import Container from "../Container";
-import CustomButtonLink from "../CustomButtonLink";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getReviewByMovie } from '../../api/review';
+import { useNotification } from '../../hooks';
+import Container from '../Container';
+import CustomButtonLink from '../CustomButtonLink';
+import RatingStar from '../RatingStar';
+
+const getNameInitial = (name = '') => {
+  return name[0].toUpperCase();
+};
 
 export default function MovieReviews() {
+  const [reviews, setReviews] = useState([]);
+  const { movieId } = useParams();
+  const { updateNotification } = useNotification();
+
+  const fetchReviews = async () => {
+    const { reviews, error } = await getReviewByMovie(movieId);
+    if (error) return updateNotification('error', error);
+
+    setReviews([...reviews]);
+  };
+
+  useEffect(() => {
+    if (movieId) fetchReviews();
+  }, [movieId]);
+
   return (
     <div className="dark:bg-primary bg-white min-h-screen pb-10">
       <Container className="xl:px-0 px-2 py-8">
@@ -10,13 +33,39 @@ export default function MovieReviews() {
           <h1 className="text-2xl font-semibold dark:text-white text-secondary">
             <span className="text-light-subtle dark:text-dark-subtle font-normal">
               Reviews for:
-            </span>{" "}
+            </span>{' '}
             This is the title
           </h1>
 
           <CustomButtonLink label="View All" />
         </div>
+
+        <div className="space-y-3 mt-3">
+          {reviews.map((review) => (
+            <ReviewCard review={review} key={review.id} />
+          ))}
+        </div>
       </Container>
     </div>
   );
 }
+
+const ReviewCard = ({ review }) => {
+  if (!review) return null;
+
+  const { owner, content, rating } = review;
+  return (
+    <div className="flex space-x-3">
+      <div className="flex items-center justify-center w-14 h-14 rounded-full bg-light-subtle dark:bg-dark-subtle text-white text-xl select-none">
+        {getNameInitial(owner.name)}
+      </div>
+      <div>
+        <h1 className="dark:text-white text-secondary font-semibold text-lg">
+          {owner.name}
+        </h1>
+        <RatingStar rating={rating} />
+        <p className="text-light-subtle dark:text-dark-subtle">{content}</p>
+      </div>
+    </div>
+  );
+};
