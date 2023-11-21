da128105d8737dbb81eacc493a9659436d2eb817
onurtaskirancom <taskiranonur9@gmail.com>
2023-11-22 02:22:49 +0300

    Route For Most Rated From All Types

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,8 +1,9 @@
-const { getAppInfo } = require("../controllers/admin");
-const { isAuth, isAdmin } = require("../middlewares/auth");
+const { getAppInfo, getMostRated } = require('../controllers/admin');
+const { isAuth, isAdmin } = require('../middlewares/auth');
 
-const router = require("express").Router();
+const router = require('express').Router();
 
-router.get("/app-info", isAuth, isAdmin, getAppInfo);
+router.get('/app-info', isAuth, isAdmin, getAppInfo);
+router.get('/most-rated', isAuth, isAdmin, getMostRated);
 
 module.exports = router;
