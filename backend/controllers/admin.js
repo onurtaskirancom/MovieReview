1e2b3f2b55ef12e39e40900e529efb424aac464e
onurtaskirancom <taskiranonur9@gmail.com>
2023-11-21 05:00:20 +0300

    Converting to Local String

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,11 +1,11 @@
-const Movie = require("../models/movie");
-const Review = require("../models/review");
-const User = require("../models/user");
+const Movie = require('../models/movie');
+const Review = require('../models/review');
+const User = require('../models/user');
 
 exports.getAppInfo = async (req, res) => {
   const movieCount = await Movie.countDocuments();
   const reviewCount = await Review.countDocuments();
   const userCount = await User.countDocuments();
 
-  res.json({ movieCount, reviewCount, userCount });
+  res.json({ appInfo: { movieCount, reviewCount, userCount } });
 };
