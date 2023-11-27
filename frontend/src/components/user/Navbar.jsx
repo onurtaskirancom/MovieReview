170c3497b842a496e5315662666e5a3daa2a179f
onurtaskirancom <taskiranonur9@gmail.com>
2023-11-27 05:50:44 +0300

    Making Navbar Responsive

--- frontend/src/components/user/Navbar.jsx
+++ frontend/src/components/user/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { BsFillSunFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
-import { useAuth, useTheme } from "../../hooks";
-import Container from "../Container";
+import React from 'react';
+import { BsFillSunFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import { useAuth, useTheme } from '../../hooks';
+import Container from '../Container';
+import AppSearchForm from '../form/AppSearchForm';
 
 export default function Navbar() {
   const { toggleTheme } = useTheme();
@@ -14,23 +15,22 @@ export default function Navbar() {
       <Container className="p-2">
         <div className="flex justify-between items-center">
           <Link to="/">
-            <img src="./logo.png" alt="" className="h-10" />
+            <img src="./logo.png" alt="" className="sm:h-10 h-8" />
           </Link>
 
-          <ul className="flex items-center space-x-4">
+          <ul className="flex items-center sm:space-x-4 space-x-2">
             <li>
               <button
                 onClick={toggleTheme}
-                className="dark:bg-white bg-dark-subtle p-1 rounded"
+                className="dark:bg-white bg-dark-subtle p-1 rounded sm:text-2xl text-lg"
               >
                 <BsFillSunFill className="text-secondary" size={24} />
               </button>
             </li>
             <li>
-              <input
-                type="text"
-                className="border-2 border-dark-subtle p-1 rounded bg-transparent text-xl outline-none focus:border-white transition text-white"
+              <AppSearchForm
                 placeholder="search..."
+                inputClassName="border-dark-subtle text-white focus:border-white sm:w-auto w-40 sm:text-lg"
               />
             </li>
             <li>
