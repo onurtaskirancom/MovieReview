470a87f1ab2bacb056a4d236ab49772fdcd5182b
onurtaskirancom <taskiranonur9@gmail.com>
2023-11-29 07:05:38 +0300

    Navigating to Search Movies

--- frontend/src/components/user/Navbar.jsx
+++ frontend/src/components/user/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BsFillSunFill } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth, useTheme } from '../../hooks';
 import Container from '../Container';
 import AppSearchForm from '../form/AppSearchForm';
@@ -9,6 +9,11 @@ export default function Navbar() {
   const { toggleTheme } = useTheme();
   const { authInfo, handleLogout } = useAuth();
   const { isLoggedIn } = authInfo;
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (query) => {
+    navigate('/movie/search?title=' + query);
+  };
 
   return (
     <div className="bg-secondary shadow-sm shadow-gray-500">
@@ -31,6 +36,7 @@ export default function Navbar() {
               <AppSearchForm
                 placeholder="search..."
                 inputClassName="border-dark-subtle text-white focus:border-white sm:w-auto w-40 sm:text-lg"
+                onSubmit={handleSearchSubmit}
               />
             </li>
             <li>
